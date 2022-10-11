e1a26077c8ac610209c8abdd3e3ceb3de768259b
NORXND <szymon.piotr.grzegorzewski@gmail.com>
2022-10-11 19:49:59 +0200

    Some reformatting, margin and internal changes

--- src/work/work.jsx
+++ src/work/work.jsx
@@ -12,65 +12,7 @@ import main_styles from '../main.scss';
 import { Trans } from 'react-i18next';
 import { Link } from 'react-router-dom';
 
-// Images
-import gt_thumbnail from '../assets/images/work_gt.webp';
-import web_norx_thumbnail from '../assets/images/work_web_norx.webp';
-import web_cd_thumbnail from '../assets/images/work_web_cd.webp';
-import yt_thumbnail from '../assets/images/work_yt.webp';
-import sc_thumbnail from '../assets/images/work_sc.webp';
-
-// Current Work Template
-const cpt = {
-	'Generation Trains': {
-		Link: 'https://www.roblox.com/games/10919455322/Generation-Trains',
-		Description:
-			'Unlimited Fun. Infinity Possibilities. Generation Trains is a Roblox Railway Simulator for Fututre Generations! Now in production.',
-		Image: gt_thumbnail,
-	},
-};
-
-// Websites Template
-const wt = {
-	NORXNDOfficialWebsite: {
-		Link: 'https://norxnd.cf',
-		Description: 'The website you see now!',
-		Image: web_norx_thumbnail,
-	},
-	ChojrakDevelopmentOfficialWebsite: {
-		Link: 'https://chojrakdev.cf/old',
-		Description: 'Old official website of Chojrak Development!',
-		Image: web_cd_thumbnail,
-	},
-};
-
-// Misc Template
-const mt = {
-	YouTube: {
-		Link: 'https://www.youtube.com/c/NORXND',
-		Description:
-			'On my official YouTube channel you can find my videos and music!',
-		Image: yt_thumbnail,
-	},
-	SoundCloud: {
-		Link: 'https://soundcloud.com/norxnd',
-		Description: 'Here you can find my music!',
-		Image: sc_thumbnail,
-	},
-};
-
-// Old Stuff Template
-const ost = {
-	Dispatch: {
-		Link: 'https://www.roblox.com/games/4962313634/Dispatch-DISCONTINUED',
-		Description: 'Dispatch - Roblox Train Simulator',
-		Image: '',
-	},
-	SoundCloud: {
-		Link: 'https://www.roblox.com/games/7127922413/Scavenge-DISCONTINUED',
-		Description: 'Scavenge - Adventure Platformer Roblox Game',
-		Image: '',
-	},
-};
+import { cpt, wt, mt, ost } from './templates';
 
 function Thumbnail(props) {
 	return (
